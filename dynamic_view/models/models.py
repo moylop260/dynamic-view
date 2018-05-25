69abe1949937807c53399ee587eae945ca53a1bd
Moisés López <moylop260@vauxoo.com>
2018-05-25 02:05:23 -0500

    [REF] view: Guidelines, order

--- dynamic_view/models/models.py
+++ dynamic_view/models/models.py
@@ -5,6 +5,7 @@ from lxml import etree
 
 from openerp import models, fields, api
 from openerp.osv.orm import setup_modifiers
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
 
 
 # class /users/moylop260/odoo/dynamic_view(models.Model):
@@ -26,9 +27,18 @@ class DynamicView(models.TransientModel):
     employee_id = fields.Many2one('hr.employee', store=False)
     dummy = fields.Char(store=False)
     # dummy2 = fields.Text(store=False)
+    field_view_definition = '<field name="%(t_date_name)s" string="%(date_name)s"/>'
+
+    @staticmethod
+    def get_field_states():
+        return fields.Selection([
+            ('present_for_duty', 'Present for Duty'),
+            ('recreational', 'Recreational'),
+        ])
 
     @api.model
-    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False,
+                        submenu=False):
         result = super(DynamicView, self).fields_view_get(
             view_id, view_type, toolbar=toolbar, submenu=submenu)
         if view_type == 'tree':
@@ -36,20 +46,18 @@ class DynamicView(models.TransientModel):
             node = doc.xpath("//field[@name='dummy']")[0]
             parent_node = node.getparent()
             date_work = datetime.now()
-            def get_states_field():
-                return fields.Selection([
-                        ('present_for_duty', 'Present for Duty'),
-                        ('recreational', 'Recreational'),
-                    ], store=False, default='present_for_duty')
             for i in range(10):
                 date_work += timedelta(days=1)
                 date_name = date_work.strftime('%b %d')
-                t_date_name = date_name.lower().replace(' ', '_')
-                self._add_field(t_date_name, get_states_field())
-                date_node = etree.fromstring('<field name="%s" widget="selection" string="%s"/>' % (t_date_name, date_name))
-                parent_node.insert(1, date_node)
-            result['arch'], result['fields'] = self.env['ir.ui.view'].postprocess_and_fields(self._name, doc, None)
-
+                t_date_name = date_work.strftime(DEFAULT_SERVER_DATE_FORMAT)
+                self._add_field(t_date_name, DynamicView.get_field_states())
+                date_node = etree.fromstring(
+                    DynamicView.field_view_definition % {
+                        't_date_name': t_date_name, 'date_name': date_name})
+                parent_node.insert(-1, date_node)
+            result['arch'], result['fields'] = (
+                self.env['ir.ui.view'].postprocess_and_fields(
+                    self._name, doc, None))
         return result
 
     @api.model
