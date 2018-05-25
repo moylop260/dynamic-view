22c9e50a8651ea3e0ede707095fc3b2f932484e7
Moisés López <moylop260@vauxoo.com>
2018-05-25 02:15:28 -0500

    [REF] good order, remove dummy unused field, add default and string from field definition

--- dynamic_view/models/models.py
+++ dynamic_view/models/models.py
@@ -25,16 +25,14 @@ class DynamicView(models.TransientModel):
     # _auto = False
 
     employee_id = fields.Many2one('hr.employee', store=False)
-    dummy = fields.Char(store=False)
-    # dummy2 = fields.Text(store=False)
-    field_view_definition = '<field name="%(t_date_name)s" string="%(date_name)s"/>'
+    field_view_definition = '<field name="%s"/>'
 
     @staticmethod
-    def get_field_states():
+    def get_field_states(label):
         return fields.Selection([
             ('present_for_duty', 'Present for Duty'),
             ('recreational', 'Recreational'),
-        ])
+        ], default='present_for_duty', string=label)
 
     @api.model
     def fields_view_get(self, view_id=None, view_type='form', toolbar=False,
@@ -43,18 +41,19 @@ class DynamicView(models.TransientModel):
             view_id, view_type, toolbar=toolbar, submenu=submenu)
         if view_type == 'tree':
             doc = etree.XML(result['arch'])
-            node = doc.xpath("//field[@name='dummy']")[0]
+            node = doc.xpath("//field[@name='employee_id']")[0]
             parent_node = node.getparent()
             date_work = datetime.now()
+            node_work = node
             for i in range(10):
-                date_work += timedelta(days=1)
                 date_name = date_work.strftime('%b %d')
                 t_date_name = date_work.strftime(DEFAULT_SERVER_DATE_FORMAT)
-                self._add_field(t_date_name, DynamicView.get_field_states())
+                self._add_field(t_date_name, DynamicView.get_field_states(date_name))
                 date_node = etree.fromstring(
-                    DynamicView.field_view_definition % {
-                        't_date_name': t_date_name, 'date_name': date_name})
-                parent_node.insert(-1, date_node)
+                    DynamicView.field_view_definition % t_date_name)
+                parent_node.insert(parent_node.index(node) + 1, date_node)
+                date_work += timedelta(days=1)
+                node_work = date_node
             result['arch'], result['fields'] = (
                 self.env['ir.ui.view'].postprocess_and_fields(
                     self._name, doc, None))
