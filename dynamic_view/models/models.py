154881e382f7d1c07a15f48671c0dbffcc063792
Moisés López <moylop260@vauxoo.com>
2018-05-25 01:42:13 -0500

    working phase 1

--- dynamic_view/models/models.py
+++ dynamic_view/models/models.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime, timedelta
 from lxml import etree
 
 from openerp import models, fields, api
 from openerp.osv.orm import setup_modifiers
+
 
 # class /users/moylop260/odoo/dynamic_view(models.Model):
 #     _name = '/users/moylop260/odoo/dynamic_view./users/moylop260/odoo/dynamic_view'
@@ -21,53 +23,33 @@ class DynamicView(models.TransientModel):
     _name = 'dynamic.view'
     # _auto = False
 
-    dummy = fields.Text(store=False)
-    dummy2 = fields.Text(store=False)
-    # custom = fields.Text(store=False)
+    employee_id = fields.Many2one('hr.employee', store=False)
+    dummy = fields.Char(store=False)
+    # dummy2 = fields.Text(store=False)
 
     @api.model
     def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
         result = super(DynamicView, self).fields_view_get(
             view_id, view_type, toolbar=toolbar, submenu=submenu)
-        if view_type == 'form':
+        if view_type == 'tree':
             doc = etree.XML(result['arch'])
             node = doc.xpath("//field[@name='dummy']")[0]
-            node.set('string', 'Dummy 2?')
             parent_node = node.getparent()
-            setup_modifiers(node, result['fields']['dummy'])
-            node2 = etree.fromstring('<field name="dummy2"/>')
-            result['fields']['dummy2'] = result['fields']['dummy']
-            result['fields']['dummy2']['string'] = 'Dummy2'
-            print result['fields']
-            setup_modifiers(node, result['fields']['dummy2'])
-            parent_node.insert(1, node2)
+            date_work = datetime.now()
+            def get_states_field():
+                return fields.Selection([
+                        ('present_for_duty', 'Present for Duty'),
+                        ('recreational', 'Recreational'),
+                    ], store=False, default='present_for_duty')
+            for i in range(10):
+                date_work += timedelta(days=1)
+                date_name = date_work.strftime('%b %d')
+                t_date_name = date_name.lower().replace(' ', '_')
+                self._add_field(t_date_name, get_states_field())
+                date_node = etree.fromstring('<field name="%s" widget="selection" string="%s"/>' % (t_date_name, date_name))
+                parent_node.insert(1, date_node)
+            result['arch'], result['fields'] = self.env['ir.ui.view'].postprocess_and_fields(self._name, doc, None)
 
-            import pdb;pdb.set_trace()
-            custom2 = fields.Text(store=False)
-            # self._fields.update({'custom2': custom2})
-            self._add_field('custom2', custom2)
-            node_custom = etree.fromstring('<field name="custom2"/>')
-            result['fields']['custom2'] = result['fields']['dummy']
-            result['fields']['custom2']['string'] = 'Custom 2'
-            setup_modifiers(node_custom, result['fields']['custom2'])
-            parent_node.insert(1, node_custom)
-
-
-        # asset_id = self.env.context.get('active_id')
-        # active_model = self.env.context.get('active_model')
-        # if active_model == 'account.asset.asset' and asset_id:
-        #     asset = self.env['account.asset.asset'].browse(asset_id)
-        #     doc = etree.XML(result['arch'])
-        #     if asset.method_time == 'number' and doc.xpath("//field[@name='method_end']"):
-        #         node = doc.xpath("//field[@name='method_end']")[0]
-        #         node.set('invisible', '1')
-        #         setup_modifiers(node, result['fields']['method_end'])
-        #     elif asset.method_time == 'end' and doc.xpath("//field[@name='method_number']"):
-        #         node = doc.xpath("//field[@name='method_number']")[0]
-        #         node.set('invisible', '1')
-        #         setup_modifiers(node, result['fields']['method_number'])
-        #     result['arch'] = etree.tostring(doc)
-            result['arch'] = etree.tostring(doc)
         return result
 
     @api.model
